65192e94b3f8deb455964b4a920e5ffb87a8c2c1
sky121016 <sky121016@naver.com>
2025-05-18 19:18:18 +0900

    feat: 홈화면 UI 구현

---
+++ src/components/home/Window.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import search from "../../assets/search.svg"
+
+const Circle = ({color}) =>{
+    return(
+        <div className='rounded-full w-12 h-12' style={{background:`var(--color-${color})`}}></div>
+    )
+}
+
+const Window = () => {
+    const today = new Date();
+    const formattedDate = `${today.getFullYear()}년 ${today.getMonth()+1}월 ${today.getDate()}일`;
+
+    const todayQuestion = "프로세스와 스레드의 차이를 설명하세요.";
+
+  return (
+    <div className="w-640 h-360 bg-white rounded-lg shadow-[0px_0px_15px_0px_rgba(29,78,214,0.15)] mb-50">
+      {/* header */}
+      <div className="border-b-1 w-full h-42 border-gray-100 flex items-center px-24">
+        {/* circles */}
+        <div className="flex gap-6 items-center pr-117">
+          <Circle color="primary" />
+          <Circle color="secondary" />
+          <Circle color="gray-100" />
+        </div>
+
+        {/* search */}
+        <div className="w-270 py-4 bg-gray-100 rounded-[5px] flex justify-end px-12">
+          <p className="text-center text-sm text-gray-700">{formattedDate}</p>
+          <img src={search} alt="search" className="pl-60" />
+        </div>
+      </div>
+
+      {/* content */}
+      <div className='py-22 flex flex-col items-center px-48 gap-52'>
+        <span className='text-base text-center font-semibold text-black border-b-1 border-black'>오늘의 CS 질문</span>
+        <p className='text-center text-5xl font-semibold text-primary text-shadow-[2px_2px_10px_rgb(0_0_0_/_0.15)] leading-60'>{todayQuestion}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Window
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Nav from '../components/global/Nav'
+import React from "react";
+import Nav from "../components/global/Nav";
+import BigButton from "../components/global/BigButton";
+import { Link } from "react-router-dom";
+import Window from "../components/home/Window";
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center">
+      {/* gradient */}
+      <div className="w-full py-50 bg-linear-to-b from-white to-secondary flex flex-col justify-center items-center">
+        <Window />
 
+        <div className="flex justify-center">
+          <Link className="items-center px-50 py-16 text-lg rounded-[10px] text-white bg-primary">
+            답변 작성하기
+          </Link>
+        </div>
+      </div>
+
+      <p className="text-gray-500 text-center py-24">
+        매일 새로운 문제가 생성됩니다.
+      </p>
+
+      {/* CS 면접 질문 리스트 */}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
