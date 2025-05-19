d58c86de357af293c0174757c3abdd53d337e925
엄태범 <djaxoqja8@gmail.com>
2025-05-19 17:03:39 +0900

    탈퇴 페이지 추가 및 각종 페이지 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Nav from "../components/global/Nav";
 import BigButton from "../components/global/BigButton";
 import { Link } from "react-router-dom";
 import Window from "../components/home/Window";
-import axios from "axios";
 
 const Home = () => {
-  const [todayQuestion, setTodayQuestion] = useState();
-  const fetchTodayQuestion = async()=>{
-    try{
-      const res = await axios.get('/api/questions/today');
-      console.log(res.data);
-      setTodayQuestion(res.data.result);
-    }catch(e){
-      console.log(e);
-    }
-  }
-
-  useEffect(()=>{
-    fetchTodayQuestion();
-  },[]);
-  
   return (
     <div className="flex flex-col justify-center">
       {/* gradient */}
       <div className="w-full py-50 bg-linear-to-b from-white to-secondary flex flex-col justify-center items-center">
-        <Window todayQuestion={todayQuestion}/>
+        <Window />
 
         <div className="flex justify-center">
-          {todayQuestion && 
-          <Link className="items-center px-50 py-16 text-lg rounded-[10px] text-white bg-primary" to={`answers/post/${todayQuestion.id}`}>
+          <Link className="items-center px-50 py-16 text-lg rounded-[10px] text-white bg-primary">
             답변 작성하기
           </Link>
-          }
         </div>
       </div>
 
