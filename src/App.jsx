0571d3eb9885be7693b7ee6803f0016d03b492d4
djaxoqja <163632316+djaxoqja@users.noreply.github.com>
2025-05-19 09:16:59 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
 import Nav from "./components/global/Nav";
+import InfoUpdate from "./pages/InfoUpdate";
+import QuestionBoard from './pages/QuestionBoard';
+import QuestionDetail from './pages/QuestionDetail';
+
 
 function App() {
   return (
@@ -10,6 +15,10 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/user/info" element={<MyPage />} />
+          <Route path="/user/update" element={<InfoUpdate />} />
+          <Route path="/comm" element={<QuestionBoard />} />
+          <Route path="/comm/all" element={<QuestionDetail />} />
         </Routes>
       </BrowserRouter>
     </>
