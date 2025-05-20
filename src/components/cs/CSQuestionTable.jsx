03d1d3494e69cf5d99a3cefd5f81b89c35313f05
sky121016 <sky121016@naver.com>
2025-05-20 16:18:34 +0900

    feat: CSQuestionTable 테이블 구조 수정

--- src/components/cs/CSQuestionTable.jsx
+++ src/components/cs/CSQuestionTable.jsx
@@ -1,36 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CSQuestionTable = ({ questions }) => {
   if (!questions) {
     return null;
   }
   const thStyle = "font-medium py-10";
+
   return (
     <div>
       <table className="w-full">
-        <tr className="bg-secondary rounded-[5px] text-center">
-          <th className={`${thStyle} rounded-l-[5px] w-90`}>번호</th>
-          <th className={`${thStyle} text-start`}>문제</th>
-          <th className={thStyle}>날짜</th>
-          <th className={`${thStyle} rounded-r-[5px]`}>정보</th>
-        </tr>
-        {questions.map((q) => {
-          return (
-            <tr
-              key={q.id}
-              className="h-48 text-center border-b-1 border-gray-300"
-            >
-              <td>{q.id}</td>
-              <td className="text-start">{q.content}</td>
-              <td>{q.createdAt.substring(0, 10)}</td>
-              <td className="flex justify-center items-center h-48">
-                <p className="bg-gray-700 text-white w-48 h-28 flex justify-center items-center rounded-sm text-sm">
-                  {q.submitted ? "제출" : "미제출"}
-                </p>
-              </td>
-            </tr>
-          );
-        })}
+        <thead>
+          <tr className="bg-secondary rounded-[5px] text-center">
+            <th className={`${thStyle} rounded-l-[5px] w-90`}>번호</th>
+            <th className={`${thStyle} text-start`}>문제</th>
+            <th className={thStyle}>날짜</th>
+            <th className={`${thStyle} rounded-r-[5px]`}>정보</th>
+          </tr>
+        </thead>
+        <tbody>
+          {questions.map((q) => {
+            return (
+              <tr
+                key={q.id}
+                className="h-48 text-center border-b-1 border-gray-300"
+              >
+                <td>{q.id}</td>
+                <td className="text-start">
+                  <Link to={`/questions/detail/${q.id}`}>{q.content}</Link>
+                </td>
+                <td>{q.createdAt.substring(0, 10)}</td>
+                <td className="flex justify-center items-center h-48">
+                  <p className="bg-gray-700 text-white w-48 h-28 flex justify-center items-center rounded-sm text-sm">
+                    {q.submitted ? "제출" : "미제출"}
+                  </p>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
