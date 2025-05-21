5b1b65ec74bb40260b2a2e627a6dd15af6d0e19a
sky121016 <sky121016@naver.com>
2025-05-21 11:20:15 +0900

    feat: Tab 바 수정

--- src/components/global/Tab.jsx
+++ src/components/global/Tab.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function AnswerTab({ to, label, active = false }) {
+function AnswerTab({ to, label, active = false, state }) {
   return (
     <Link
       to={to}
+      state={state}
       className={`inline-block px-8 py-4 min-w-[80px] text-center text-lg font-medium
         ${
           active
@@ -17,17 +18,41 @@ function AnswerTab({ to, label, active = false }) {
   );
 }
 
-const Tab = ({ questionId }) => {
+const Tab = ({ title, titleTo, from }) => {
+  const location = useLocation();
+
+  let leftLabel = "";
+  let leftTo = "";
+  let stateForMyAnswers = {};
+
+  if (from === "question") {
+    leftLabel = title;
+    leftTo = titleTo;
+    stateForMyAnswers = {
+      from: "question",
+      title,
+      titleTo,
+      questionId: title.replace("번", ""),
+    };
+  } else if (from === "myPage") {
+    leftLabel = "마이페이지";
+    leftTo = "/user/info";
+    stateForMyAnswers = {
+      from: "myPage",
+    };
+  }
   return (
     <nav className="mt-24">
       <AnswerTab
-        to={`/questions/detail/${questionId}`}
-        label={`${questionId}번`}
-        active
+        to={leftTo}
+        label={leftLabel}
+        active={location.pathname === leftTo}
       />
+
       <AnswerTab
         to="/myAnswers"
         label="내 답변"
+        state={stateForMyAnswers}
         active={location.pathname === "/myAnswers"}
       />
     </nav>
