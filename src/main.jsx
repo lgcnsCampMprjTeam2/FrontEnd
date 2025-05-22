0a3bed38ae2093ed56767ec28b22dd6c25901235
dldh1218 <dldh1218@gmail.com>
2025-05-22 18:38:09 +0900

    Reapply "Merge branch 'main' of https://github.com/lgcnsCampMprjTeam2/FrontEnd into feat/#103"
    This reverts commit fcd663080dd3dc26e2a4ba5435369cd62abbb035.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "./api/axiosSetup.js"
 
 createRoot(document.getElementById('root')).render(
     <App />
