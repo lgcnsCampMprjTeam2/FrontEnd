8d02925772dcc96d16ed139702c90a84f9661c32
엄태범 <djaxoqja8@gmail.com>
2025-05-22 23:37:43 +0900

    feat: 질문 게시판 상세 조회 페이지, 스타일  수정

--- src/pages/QuestionDetail.jsx
+++ src/pages/QuestionDetail.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import {
   getQuestionDetail,
   getComments,
@@ -108,7 +110,7 @@ const QuestionDetail = () => {
       deleteQuestion(number)
         .then(() => {
           alert('질문이 삭제되었습니다.');
-          navigate('/');
+          navigate('/comm');
         })
         .catch((err) => {
           console.error(err);
@@ -155,17 +157,27 @@ const QuestionDetail = () => {
           </table>
 
           {/* 질문 내용 */}
-          <div className="content-box border p-4 mb-6 whitespace-pre-line">
+          <div className="content-box border p-4 mb-6">
             {isEditing ? (
-              <textarea
-                value={editedContent}
-                onChange={(e) => setEditedContent(e.target.value)}
-                className="w-full h-40 p-2 border rounded"
+              <CKEditor
+                editor={ClassicEditor}
+                data={editedContent}
+                onReady={(editor) => {
+                  const editable = editor.ui.view.editable.element;
+                  editable.style.minHeight = '100px';
+                  editable.style.maxHeight = '100px';
+                  editable.style.padding = '1rem';
+                }}
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  setEditedContent(data);
+                }}
               />
             ) : (
-              questionInfo.content
+              <div dangerouslySetInnerHTML={{ __html: questionInfo.content }} />
             )}
           </div>
+
 
           {/* 액션 버튼 */}
           <div className="actions flex gap-4 mb-6">
