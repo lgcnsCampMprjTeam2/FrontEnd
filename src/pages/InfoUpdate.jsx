e563cdc94e1d04aa14cc19929e23ca03ea3d2d74
sky121016 <sky121016@naver.com>
2025-05-22 12:06:10 +0900

    feat: 회원 정보 수정

--- src/pages/InfoUpdate.jsx
+++ src/pages/InfoUpdate.jsx
@@ -1,56 +1,86 @@
 import React, { useState, useEffect } from "react";
-import '../styles/style.css';
+import "../styles/style.css";
 import BigButton from "../components/global/BigButton";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { postLogout } from "../api/authApi";
 
 function InfoUpdate() {
   const navigate = useNavigate();
 
   // userInfo 상태 생성 (초기값은 빈 객체)
-  const [userInfo, setUserInfo] = useState({});
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [nickname, setNickname] = useState(localStorage.getItem("nickname"));
+  const [newProfileimage, setNewProfileImage] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
 
   // 컴포넌트 마운트 시 public/UserInfoData.json 파일 fetch
-  useEffect(() => {
-    fetch('/UserInfoData.json')
-      .then(res => res.json())
-      .then(data => setUserInfo(data))
-      .catch(err => console.error('유저 정보 로드 실패:', err));
-  }, []);
+  // useEffect(() => {
+
+  // fetch('/UserInfoData.json')
+  //   .then(res => res.json())
+  //   .then(data => setUserInfo(data))
+  //   .catch(err => console.error('유저 정보 로드 실패:', err));
+  // }, []);
+
+  const formData = new FormData();
+  formData.append("currentPassword", currentPassword);
+  formData.append("newPassword", newPassword);
+  formData.append("nickname", nickname);
+
+  if (newProfileimage) {
+    formData.append("newProfileimage", newProfileimage);
+  }
 
   // 수정 완료 버튼 클릭 시 유저정보 post 후 유저 정보 페이지로 이동동
   const handleSubmit = async (e) => {
-  e.preventDefault();
+    const accessToken = localStorage.getItem("accessToken");
+    e.preventDefault();
 
-  const formData = {
-    name: e.target.name.value,
-    birthDate: e.target.birth.value,
-    email: e.target.email.value,
-    nickname: e.target.nickname.value,
+    try {
+      const res = await axios.post("/api/user/update", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      console.log(res.data);
+      alert("회원 정보가 수정되었습니다. 다시 로그인 해주세요.");
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("name");
+      localStorage.removeItem("email");
+      localStorage.removeItem("nickname");
+      localStorage.removeItem("profileImgUrl");
+
+      navigate("/auth");
+      window.location.reload();
+    } catch (error) {
+      alert("회원 정보 수정에 실패했습니다.");
+    }
   };
 
-  try {
-    const response = await fetch('/api/user/update', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
+  const inputStyle =
+    "w-full px-10 py-10 border rounded-md focus:outline-none focus:ring-2 focus:ring-secondary bg-white mb-16";
 
-    if (!response.ok) {
-      throw new Error('업데이트 실패');
+  const labelStyle = "w-full block mb-4";
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setNewProfileImage(file);
+
+    // 미리보기 생성
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewUrl(reader.result); // Base64 URL 설정
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setPreviewUrl(null);
     }
-
-    const result = await response.json();
-    alert(result.message);
-
-    navigate("/user/info");
-
-  } catch (error) {
-    alert('회원 정보 수정에 실패했습니다.');
-  }
-};
-
+  };
 
   return (
     <div className="bg-white min-h-screen flex items-center justify-center">
@@ -60,19 +90,19 @@ function InfoUpdate() {
       >
         <h2 className="text-2xl font-bold mb-6 text-center">회원 정보 수정</h2>
 
-        <label className="w-full block mb-2 font-semibold" htmlFor="name">
+        {/* <label className="w-full block mb-2 font-semibold" htmlFor="name">
           이름
         </label>
         <input
           type="text"
           id="name"
           name="name"
-          defaultValue={userInfo.name || ""}
+          value={nickname}
           required
           className="w-full p-2 mb-4 border border-gray-300 rounded bg-white"
-        />
+        /> */}
 
-        <label className="w-full block mb-2 font-semibold" htmlFor="email">
+        {/* <label className="w-full block mb-2 font-semibold" htmlFor="email">
           이메일
         </label>
         <input
@@ -82,19 +112,57 @@ function InfoUpdate() {
           defaultValue={userInfo.email || ""}
           required
           className="w-full p-2 mb-4 border border-gray-300 rounded bg-white"
-        />
+        /> */}
 
-        <label className="w-full block mb-2 font-semibold" htmlFor="nickname">
+        <label className={labelStyle} htmlFor="currentPassword">
+          현재 비밀번호
+        </label>
+        <input
+          type="password"
+          id="currentPassword"
+          name="currentPassword"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+          required
+          className={inputStyle}
+        />
+        <label className={labelStyle} htmlFor="currentPassword">
+          새 비밀번호
+        </label>
+        <input
+          type="password"
+          id="newPassword"
+          name="newPassword"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          required
+          className={inputStyle}
+        />
+        <label className={labelStyle} htmlFor="nickname">
           닉네임
         </label>
         <input
           type="text"
           id="nickname"
           name="nickname"
-          defaultValue={userInfo.nickname || ""}
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
           required
-          className="w-full p-2 mb-6 border border-gray-300 rounded bg-white"
+          className={inputStyle}
         />
+        <label className={labelStyle} htmlFor="newProfileimage">
+          프로필 이미지
+        </label>
+        <input
+          type="file"
+          accept="image/*"
+          id="newProfileimage"
+          name="newProfileimage"
+          onChange={(e) => handleImageChange(e)}
+          className={inputStyle}
+        />
+
+        {previewUrl && <img src={previewUrl} alt="profileimg" className="w-70 h-70" />}
 
         <div className="text-center mt-40">
           <BigButton text="수정완료" fill />
