fcd663080dd3dc26e2a4ba5435369cd62abbb035
dldh1218 <dldh1218@gmail.com>
2025-05-22 18:37:54 +0900

    Revert "Merge branch 'main' of https://github.com/lgcnsCampMprjTeam2/FrontEnd into feat/#103"
    This reverts commit b7c56cd89f9d493f68e4a08812ff0664fa9f1bf7, reversing
    changes made to 9af08efcf9c13b425fe79222f33997f5db7dd6ef.

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -24,32 +24,3 @@ export const postLogout = async ({ accessToken }) => {
     console.log(e);
   }
 };
-function getCookie(name) {
-  const cookieArr = document.cookie.split("; ");
-  for (const cookie of cookieArr) {
-    const [key, value] = cookie.split("=");
-    if (key === name) return value;
-  }
-  return null;
-}
-export const reissueToken = async () =>{
-  const refreshToken = getCookie("refreshToken");
-  try{
-    const res = await axios.post(
-      "/api/auth/reissue",
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${refreshToken}`,
-        },
-        withCredentials: true,
-      }
-    );
-
-    localStorage.setItem("accessToken", res.data.result);
-
-    return res;
-  }catch(e){
-    console.log(e);
-  }
-}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "./api/axiosSetup.js"
 
 createRoot(document.getElementById('root')).render(
     <App />
